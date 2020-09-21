a5af2ae8fad935990dd2c67c3d55f366194eef59
Yunru Pan <yunrupan@YunrudeMacBook-Pro.local>
2020-09-21 22:05:40 +0800

    mini project 1st

--- projects/mini-project1/script.js
+++ projects/mini-project1/script.js
@@ -2,9 +2,9 @@
 // let radius = 2;
 // let max = 800;
 // createRandomNodes(100,2,800);
-let num = Math.floor(Math.random()*100);
-let yPos = Math.floor(Math.random()*1000);
-let xPos = Math.floor(Math.random()*1200);
+let num = 30;
+let yPos = 0;
+let xPos = 0;
 createRandomImage(30, 0, 0);
 
 let clicking = document.getElementById("reset");
@@ -13,22 +13,25 @@ clicking.addEventListener("click",function(){
 });
 
 function createRandomImage(num, xPos, yPos){
-  let canvas = document.getElementById("myCanvas");
-  let context = canvas.getContext("2d");
+  var canvas = document.getElementById("myCanvas");
+  var context = canvas.getContext("2d");
   context.clearRect(0,0,canvas.width,canvas.height);
 
-  var image= new Image();
-  image.src='kendall.jpg';
-  image.style.width='50px'
-  image.style.height='auto';
-  image.onload= function(){
-    for(var i= 0; i<= num; i++){
-      // content.beginPath();
-      context.drawImage(image,xPos,yPos);
-      // context.fill();
-      // context.closePath();
-    }
-  }
+  var added = Math.floor(Math.random(num)*10);
+  num+=added;
+
+  for(var i= 0; i<= num; i++){
+    var image= new Image();
+    image.src='2.JPG';
+    image.style.width='30px'
+    image.style.height='30px';
+    let random_yPos = Math.floor(Math.random(yPos)*1000);
+    let random_xPos = Math.floor(Math.random(xPos)*1000);
+    context.drawImage(image,random_xPos,random_yPos);
+
+    document.body.appendChild(image);
+   }
+   // }
   // let canvas= document.getElementById("myCanvas");
   // let context = canvas.getContext("2d");
   // context.clearRect(0,0, canvas.width,canvas.height);
