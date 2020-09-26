b66b15d9375f6159368cc075b934d937e05a012c
Yunru Pan <yunrupan@YunrudeMacBook-Pro.local>
2020-09-26 16:41:46 +0800

    push

--- projects/experiment/pop-up/script.js
+++ projects/experiment/pop-up/script.js
@@ -10,20 +10,22 @@ function openWindow(){
   let randomY = Math.random()*(sh-400);
   let specs = "width=200, height=100, left="+randomX+", top="+randomY;
   console.log(specs);
+
   let newWindow= window.open("","",specs);
+
+  let randomTime =5000+Math.random()*4000;
   // make the new window
   setTimeout(function(){
     newWindow.close();
-  },2000);
-
-
-
+  },randomTime);
   // or set up another popup.html, put the "popup.html" in the first ""
 }
+
+
 function openManyWindows(){
-  for(let i= 0;i<5; i++){
+  for(let i= 0;i<10; i++){
     openWindow();
   }
 }
 
-button.addEventListener("click",openWindow)
+button.addEventListener("click",openManyWindows())
