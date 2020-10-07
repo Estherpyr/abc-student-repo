d3f8b9b74ff35059257728fc50cb625446bf644f
Yunru Pan <yunrupan@YunrudeMacBook-Pro.local>
2020-10-07 13:09:49 +0800

    mini project 3

---
+++ projects/mini-project3/script.js
@@ -0,0 +1,17 @@
+// let moveText = document.getElementById("src-1");
+//
+// setInterval(()=>{
+//   for(let i = 0; i<10; i++){
+//    let ranX = Math.random()*(window.innerWidth-50);
+//    let ranY = Math.random()*(window.innerHeight-50);
+//    moveText[i].style.transform="translate("+ranX+"px, "+ranY+"px)";
+//  }
+// },2000);
+//
+// setTimeout(()=>{
+//   for(let i = 0; i<10; i++){
+//     let ranX = Math.random()*(window.innerWidth-50);
+//     let ranY = Math.random()*(window.innerHeight-50);
+//     moveText[i].style.transform = "translate("+ranX+"px, "+ranY+"px)";
+//   }
+// }, 0)
